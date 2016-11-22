1e08216827c174ce4a570708f94cae64bc26ec86
chenwei <cheenwe@gmail.com>
2016-11-22 23:13:42 +0800

    deving

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,15 @@ module Imerp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Ensure App config files exist.
+    if Rails.env.development?
+      %w(settings).each do |filename|
+        filename = "config/#{filename}.yml"
+        next if File.exist?(Rails.root.join(filename))
+        FileUtils.cp(Rails.root.join("#{filename}.example"), Rails.root.join(filename))
+      end
+    end
+
   end
 end
